794693c32b58dca9d2692caa5e6e80157f524852
Tsubasa <remmajitotoi@gmail.com>
2025-05-07 13:12:56 +0900

    fix: YouTubeApiResponse型を追加

--- src/app/types.ts
+++ src/app/types.ts
@@ -11,8 +11,46 @@ export interface YouTubeVideo {
   lastUpdated?: string;        // 最終更新日時
 }
 
+export interface YouTubeApiResponse {
+  kind: string;
+  etag: string;
+  nextPageToken?: string;
+  prevPageToken?: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: YouTubeApiItem[];
+}
+
+export interface YouTubeApiItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: { url: string; width: number; height: number };
+      medium: { url: string; width: number; height: number };
+      high: { url: string; width: number; height: number };
+    };
+    channelTitle: string;
+    liveBroadcastContent: string;
+    publishTime: string;
+  };
+  statistics: {
+    viewCount: string;
+    likeCount: string;
+    favoriteCount: string;
+    commentCount: string;
+  };
+}
+
 // 初星学園のYouTubeチャンネルID
-export const HATSUHOSHI_CHANNEL_ID = 'UC2dXx-3RXeeP8hA5AGt8vuw';
+export const HATSUHOSHI_CHANNEL_ID = 'UCQhmyTiv09WYRw5hyClU4CQ';
 
 // 初星学園の音楽プレイリストID
-export const HATSUHOSHI_MUSIC_PLAYLIST_ID = 'PL8AmPgz38WkXIiEnqf-Q5XkWpQgm5UiuF'; 
+export const HATSUHOSHI_MUSIC_PLAYLIST_ID = 'PLQhmyTiv09WYRw5hyClU4CQ'; 
